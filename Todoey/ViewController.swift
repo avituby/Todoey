ff78144e630bab2c7b88c568ff1e4e5492f43e93
Avi Tuby <avi@ivory-sw.com>
2019-06-07 09:57:57 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by DAVID AVI TUBI on 07/06/2019.
+//  Copyright © 2019 DAVID AVI TUBI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
